37bf949744b1753439d0d6fb3e49ade12b9bce37
unknown <mfvl.1186@gmail.com>
2022-03-05 23:00:51 -0300

    ABP6 funcionando perfectirijillo!

--- Módulo 3/Enviados/ABP6-MFVL.py
+++ Módulo 3/Enviados/ABP6-MFVL.py
@@ -1,23 +1,14 @@
-""" En esa ocasión se solicita un programa que:
-- Pregunta el nombre de usuario y una contraseña.
-- Almacene ambos datos en una variable.
-- Que obtenga la edad del usuario a través de la consola. Sólo acepta números enteros.
-- Almacene el dato edad a cada usuario. 
-- Imprima por cada usuario: su edad, y contraseña con un desfase de 5 segundos. 
-El programa debe reiniciarse cada vez que termina. Sin perder la información anterior, debe continuar 
-preguntando por nombre y contraseña.
-Este loop podrá ser terminado sólo ingresando ‘salir’. Al momento de terminar, el programa debe imprimir 
-en pantalla la variable completa de datos hasta el momento de recibir la instrucción ‘salir’. """
 
 import re
 import time
 
+#Spacing sentences so my teacher doesn't go into OCD.
 print("")
 print ('1. Programa para loguear usuarios')
 
 users={}
 
-#Identify customer.
+#Identify new customer.
 def sign_in():
     user_name= input('Ingrese Nombre de Usuario:  ').capitalize ()
     print(f'¡Bienvenid@ {user_name}!')
@@ -43,33 +34,15 @@ def user_age():
             print("Vuelve a registrarte.")
             return continue_or_exit()
 
-# def user_age():
-#     i=0
-#     while i<3:
-#         i+=1
-#         print('Ingrese su edad')
-#         age= int(input('(Sólo dígitos)  '))
-#         if age is int:
-#             print("Edad ingresada correctamente.")  
-#             return age
-#         elif age not is int:
-#             print("Debes ingresar sólo números.")
-#         elif i==3:
-#             print("Se acabaron tu número de intentos.")
-#             print("Vuelve a registrarte.")
 
 def password_validation():
-    val=False
     i=0
     print('Ahora crea tu contraseña.')
     print('Debe contener entre 8 y 11 caracteres incluyendo mayúsculas, minúsculas y números.')
-    while i<4 and val==False:
+    while i <4:
         i+=1
-        if i==4:
-            print("Se acabaron tu número de intento.")
-            print("Vuelve a registrarte.")
-            break
         password= input('Crear Contraseña:  ')
+        val=True
         if len(password) < 8:
             print('Recuerda que son mínimo 8 caracteres')
             val = False
@@ -77,15 +50,23 @@ def password_validation():
             print('Recuerda que son máximo 11 caracteres')
             val = False
         if not re.search("[0-9]", password):
-            print('No olvides incluir al menos un número.')
+            print('Debes incluir al menos un número.')
             val = False
         if not re.search("[A-Z]", password):
-            print('No olvides incluir al menos una letra en Mayúscula.')
+            print('Debes incluir al menos una letra en Mayúscula.')
             val = False
         if not re.search("[a-z]", password):
-            print('No olvides incluir al menos una letra en minúscula.')
+            print('Debes incluir al menos una letra en minúscula.')
             val = False
-        return password
+        elif val:
+            print('')
+            print('¡Usuario creado con éxito!')
+            print('')
+            return password
+        if i==3:
+            print("Se acabaron tu número de intentos.")
+            print("Vuelve a registrarte.")
+            return continue_or_exit()
 
 
 def continue_or_exit():
@@ -97,26 +78,27 @@ def continue_or_exit():
         return False
     else:
         print("Respuesta Inválida")
+        continue_or_exit()
 
 
 def updated_users(users_dict):
     print("Los usuarios registrados son:")
-    for user in users_dict.keys():
-        print(user+': \n       edad:',users_dict[user]['age']+', contraseña:',users_dict[user]['password'])
-        time.sleep(0.5)
+    for i in range(3):
+        for user in users_dict.keys():
+            print(user+': \n       edad:',users_dict[user]['age']+', contraseña:',users_dict[user]['password'])
+            time.sleep(0.5)
 
 
 def main():
-    name = sign_in()
-    age= user_age()
-    password=password_validation()
-    users[name]= {'age': age, 'password': password}
-    print(users)
-    print('')
-    print('¡Usuario creado con éxito!')
-    print('')
-    if continue_or_exit() ==False:
-        sign_in()
+    val=False
+    while val==False:
+        name = sign_in()
+        age= user_age()
+        password=password_validation()
+        users[name]= {'age': age, 'password': password}
+        # print(users)
+        if continue_or_exit():
+            val=True
     updated_users(users)
     print('')
     print("Programa Finalizado")
