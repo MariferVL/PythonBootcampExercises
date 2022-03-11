b9bd1a78c0516e0c705b961051da1c103e45432e
unknown <mfvl.1186@gmail.com>
2022-03-11 12:45:26 -0300

    new folder ABP+

--- M4/ABP-MFVL/Users.py
+++ M4/ABP-MFVL/Users.py
@@ -1,3 +1,5 @@
+from Verification import *
+
 """ 
 DESARROLLO
 Crear clases con sintaxis Python
@@ -14,22 +16,39 @@ Actualizando módulo anterior, Python básico:
 
 # n_users=0
 users={}
+mktg={"greatest":[],
+        "silent":[],
+        "baby_Boomers":[],
+        "gen_X": [],
+        "xennials":[],
+        "gen_Y":[],
+        "gen_Z":[]}
 
 # class Users is declared.
 class Users: 
     # constructor
-    def __init__(self):
+    def __init__(self,firstname,surname,username,password,birthDay, birthMonth,birthYear,email,phone ):
         # keys are initialized with their respective values
+        self._firstname= firstname
+        self._surname= surname
+        self.username= username
+        self.__password = password
+        self.birthDay = birthDay
+        self.birthMonth = birthMonth
+        self.birthYear = birthYear 
+        self.email= email
+        self.phone = phone
+
+    def sign_in(self):
         self._firstname= input('Ingresa tu nombre: ')
         self._surname= input('Ingresa tu apellido: ')
         self.username= input('Ingresa tu nombre de usuario: ')
-        self.password = input('Crea tu contraseña: ')
+        self.password = password_validation()
         self.birthDay = int(input('Ingresa tu día de nacimiento: '))
         self.birthMonth = int(input('Ingresa tu mes de nacimiento: ')) 
         self.birthYear = int(input('Ingresa tu año de nacimiento: ')) 
+        self.email = input('Ingresa tu número correo electrónico: ') 
         self.phone = input('Ingresa tu número de contacto: ')
-        # self.age_group= 
-
 
     def save_dict(self):
         # calling attribute __dict__ on user object
@@ -39,9 +58,10 @@ class Users:
         return users
 
     def age_group(self):
+        contact_info=(self.username,self._firstname,self._surname,self.email)
         for user, info in users.items():
                 if info["birthYear"] in range (1901,1926):
-                    print( "greatest")
+                    mktg ["greatest"].append(self)
                 elif info["birthYear"] in range (1927,1945):
                     print( "silent")
                 elif info["birthYear"] in range (1946,1964):
@@ -65,22 +85,14 @@ class Users:
                 print(f'Bienvenid@ {user_name}')
                 print('Vamos a comprar!')
 
+# Driver Code
+if __name__ == "__main__":
+    # object user of class Users.
+    user= Users()
+    user.save_dict()
+    user.age_group()
+    user.shopping()
 
-# object user of class Users.
-user= Users()
-user.save_dict()
-user.age_group()
-user.shopping()
-
-
-
-
-
-
-
-    
-
-    # 
 
     
     # #details
@@ -90,11 +102,3 @@ user.shopping()
     #     province: str
     #     country: str
 
-        # self._firstname= firstname
-        # self._surname= surname
-        # self.username= username
-        # self.__password = password
-        # self.birthDay = birthDay
-        # self.birthMonth = birthMonth
-        # self.birthYear = birthYear 
-        # self.phone = phone
